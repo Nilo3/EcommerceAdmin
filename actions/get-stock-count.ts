a6693b63503583d2d1660cf21ba743ea7e873a26
Nilo Echezarreta <nilo.echezarreta@gmail.com>
2023-08-02 20:42:21 -0300

    Darkmode and charts add

---
+++ actions/get-stock-count.ts
@@ -0,0 +1,14 @@
+import prismadb from "@/lib/prismadb";
+
+export const getStockCount = async (storeId: string) => {
+    const salesCount = await prismadb.product.count({
+        where: {
+            storeId,
+            isArchived: false
+        },
+        
+    });
+
+    
+    return salesCount;
+}
